fb84a83966d3e55004385e74fe098749e3a56c05
“lucifer2355” <dgajjar999@gmail.com>
2021-06-11 12:58:56 +0530

    set email server

--- controller/sendEmailController.js
+++ controller/sendEmailController.js
@@ -1,3 +1,16 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "../config.env" });
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
 exports.sendEmail = () => {
   console.log("send email");
 };
