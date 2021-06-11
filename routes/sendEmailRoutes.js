012a597fecc59bbfd3b6e69ec07603b0f0db01b2
“lucifer2355” <dgajjar999@gmail.com>
2021-06-11 12:40:37 +0530

    add send email router

--- routes/sendEmailRoutes.js
+++ routes/sendEmailRoutes.js
@@ -1,1 +1,8 @@
 const express = require("express");
+const { sendEmail } = require("../controller/sendEmailController");
+
+const router = express.Router();
+
+router.post("/sendEmail", sendEmail);
+
+module.exports = router;
